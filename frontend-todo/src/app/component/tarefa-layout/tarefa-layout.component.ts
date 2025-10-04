e92726602226d3024120b47fbd2bfff823d7b423
Paulo Sergio <036paulo@gmail.com>
2025-10-03 21:13:31 -0300

    feat: Ajustado com o css e criado a capa da tela

--- frontend-todo/src/app/component/tarefa-layout/tarefa-layout.component.ts
+++ frontend-todo/src/app/component/tarefa-layout/tarefa-layout.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ɵEmptyOutletComponent } from "../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
 
 @Component({
   selector: 'app-tarefa-layout',
-  imports: [],
+  imports: [ɵEmptyOutletComponent],
   templateUrl: './tarefa-layout.component.html',
   styleUrl: './tarefa-layout.component.css'
 })
 export class TarefaLayoutComponent {
-
+  @Input() title: string="";
+  @Input() textBtnPrimary: string=""
+  @Input() textBtnSecundary: string=""
 }
--- frontend-todo/src/app/pages/tarefa/tarefa.component.ts
+++ frontend-todo/src/app/pages/tarefa/tarefa.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { TarefaLayoutComponent } from '../../component/tarefa-layout/tarefa-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tarefa',
-  imports: [TarefaLayoutComponent],
+  imports: [TarefaLayoutComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './tarefa.component.html',
   styleUrl: './tarefa.component.css'
 })
 export class TarefaComponent {
+
+  tarefaForm!: FormGroup;
+
+  constructor(){
+    this.tarefaForm=new FormGroup({
+      id: new FormControl(''),
+      nome: new FormControl('',[Validators.required]),
+      descricao: new FormControl(''),
+      prioridade: new FormControl('',[Validators.required]),
+      situacao: new FormControl('',[Validators.required]),
+      dataPrevistaConclusao: new FormControl('',[Validators.required])
+    })
+  }
 
 }
